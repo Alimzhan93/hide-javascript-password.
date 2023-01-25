531dd911bb1a5cc5d45cce42d4e1732b621fff74
77001 <alimzhanzhakashev@gmail.com>
2023-01-25 12:09:49 +0600

    init comit

--- js/script.js
+++ js/script.js
@@ -2,8 +2,13 @@ function showPassword() {
     const btn = document.querySelector('.password_btn')
     const input = document.querySelector('.password_input')
 
-btn.addEventListener('click', () =>{
+btn.addEventListener('click', () => {
     btn.classList.toggle('active')
+    if(input.getAttribute('type') ==='password'){
+        input.setAttribute('type','text')
+    }else{
+        input.setAttribute('type','password')
+    }
 })
 }
 showPassword()
