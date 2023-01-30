b75690d4923b0daddb30a337e06fd624b3be39e3
77001 <alimzhanzhakashev@gmail.com>
2023-01-30 12:24:55 +0600

    init commit

--- js/tabl.js
+++ js/tabl.js
@@ -1,14 +1,16 @@
+// Нашли все заголовки табов по data атрибуту
 const tablHeaders = document.querySelectorAll('[data-tab]');
+// Нашли все контент боксы
 const contentBoxes = document.querySelectorAll('[data-tab-content]');
 
 tablHeaders.forEach(function(item){
     item.addEventListener('click', function () {
-
+        // 1. Скрыть все content box
         contentBoxes.forEach(function (item) {
             item.classList.add('hidden')
         });
 
-
+         // 2. Выбрать нужный content box и показать его
 
     const contentBox = document.querySelector('#' + this.dataset.tab)
     contentBox.classList.remove('hidden')
