3c53fb445519365c8cb285e61b7dc2842ede4ed2
77001 <alimzhanzhakashev@gmail.com>
2023-01-31 12:35:32 +0600

    init commit

--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,14 @@
-const buttonModal = document.querySelector('[data-modal-button]');
+const modalButtons = document.querySelectorAll('[data-modal-button]');
+const modalClosebuttons = document.querySelectorAll('[data-modal-close]');
+const allModals = document.querySelectorAll('[data-modal]');
 
-buttonModal.addEventListener('click',function() {
-    console.log('click')
+// Кнопки - Открыть Модалку
+modalButtons.forEach(function(item){
+    item.addEventListener('click',function(){
+        const modalId = this.dataset.modalButton
+        console.log(modalId);
+        const modal = document.querySelector('#'+ modalId)
+        console.log(modal);
+        modal.classList.remove('hidden')
+    })
 })
