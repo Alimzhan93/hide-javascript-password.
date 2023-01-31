4a159aa1d55990c40270974d4a3f9ce7cc0ec90c
77001 <alimzhanzhakashev@gmail.com>
2023-01-31 17:03:09 +0600

    init commit

--- js/modal.js
+++ js/modal.js
@@ -10,5 +10,21 @@ modalButtons.forEach(function(item){
         const modal = document.querySelector('#'+ modalId)
         console.log(modal);
         modal.classList.remove('hidden')
+        // Находим внутри открываемой модалки блок .modal-window и запрещаем ему передавать клики "наверх"
+    })
+})
+
+// Кнопки - Закрыть Модалку
+modalClosebuttons.forEach(function (item) {
+    item.addEventListener('click', function () {
+        const modal = this.closest('[data-modal]');
+        modal.classList.add('hidden');
+    })
+})
+
+// Закрытие модалок по фейду
+allModals.forEach(function(item){
+    item.addEventListener('click',function(){
+        this.classList.add('hidden')
     })
 })
